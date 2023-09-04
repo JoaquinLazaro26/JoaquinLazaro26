27208af5112337613f05e63c7f035fa334843732
JoaquinLazaro <i202116199@cibertec.edu.pe>
2023-09-04 10:10:25 -0500

    Responsive

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
 /*------------------------------*/
 
 
- $(window).load(function() {
+ 	$(window).load(function() {
 	$(".loader-item").delay(500).fadeOut();
 	$("#pageloader").delay(1000).fadeOut("slow");
 	});
